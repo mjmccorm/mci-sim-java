4cfd4f6c8f11db35e76750fbfda3312b1cc865c8
Matt McCormick <mjmccorm@gmail.com>
2014-12-06 21:37:34 -0500

    Added logging ability

--- src/MyProgram.java
+++ src/MyProgram.java
@@ -1,10 +1,31 @@
 import java.util.ArrayList;
+import java.util.logging.*;
+import java.io.IOException;
 
 public class MyProgram{
 	
 	public static void main(String[] args){
 	
+		Logger logger = Logger.getLogger("myLogger");
+		logger.setLevel(Level.CONFIG);
+		ConsoleHandler cHandler = new ConsoleHandler();
+		logger.addHandler(cHandler);
+		
+		try{
+			//filename, filesize, # of files, appendable
+			FileHandler fHandler = new FileHandler("c:\\Temp\\myapp-log.%u.%g.txt", 1024 * 1024, 10, true);
+			logger.addHandler(fHandler);
+			
+		} catch (IOException e){
+			e.printStackTrace();
+		}
+		logger.log(Level.SEVERE, "Hello logging");
+		logger.log(Level.WARNING, "Hello logging");
+		logger.log(Level.INFO, "Hello logging");
+		logger.log(Level.CONFIG, "Hello logging");
+		
 		ArrayList people = new ArrayList();
+		
 		
 		Person matt = new Person();
 		System.out.println(matt.getID() + ":" + matt.getGender() + ":" + matt.getRace() + ":" + matt.getBirthdate() + ":" + matt.getAge());
